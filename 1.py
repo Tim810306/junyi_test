1fd36e8c9a30e611c01424c038b791c1a5a4a66d
TimCheng <timazoey@gmail.com>
2020-07-23 03:13:14 +0800

    update#1

---
+++ 1.py
@@ -0,0 +1,15 @@
+#第一題a用python:
+
+def reverse(s):
+    return s[::-1]
+
+
+#第一題b用python:
+
+def reverse5(s):
+    s1 = ''
+    length = len(s) - 1
+    while length >= 0:
+        s1 = s1 + s[length]
+        length = length - 1 #還沒完成順序調整
+    return s1
